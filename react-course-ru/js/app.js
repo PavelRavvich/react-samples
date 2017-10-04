3834e79ae4d7353ce43ba0c1addda19ceaeb54ed
Pavel <triodjangopiter@yandex.ru>
2017-10-05 00:33:33 +0300

    add multicomponent sample

--- react-course-ru/js/app.js
+++ react-course-ru/js/app.js
@@ -1,4 +1,36 @@
+var News = React.createClass({
+  render: function() {
+    return (
+      <div>
+        News
+      </div>
+    );
+  }
+});
+
+var Comments = React.createClass({
+    render: function() {
+      return (
+        <div>
+          comments
+        </div>
+      );
+    }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Всем привет, я компонент App!
+        <Comments />
+        <News />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
